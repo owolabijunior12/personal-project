28105dfa8a7db3656e1083b2df1b89516b895bd0
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2023-05-05 06:49:19 +0100

    connecting to database

--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
+require("dotenv").config()
 const app = express();
 
 app.get("/", (req, res)=>{
     return res.json("hello iboytech to the world");
 })
 
+mongoose.connect(process.env.DB_STRINGS,{useNewurlParser : true});
+
+
+
+mongoose.connection
+.once("open", ()=>console.log("connected to MongoDB DataBase is connected........"))
+.on("error",(err)=>{console.log(`Not  connecting to mongoDB ${err}`)})
 app.listen(4000,()=>{
-    console.log("listening on port 4000");
+    console.log("Server started and listen to port 4000");
 })
