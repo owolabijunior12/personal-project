1a8b0c0440923683243cb96bacd855e95db1468e
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2023-05-11 07:54:29 +0100

    small changes

--- client/src/conponent/Header.jsx
+++ client/src/conponent/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {FaCrown}from 'react-icons/fa';
 import profile from '../asset/profile.jpg'
-import {useNavigate, NavLink} from 'react-router-dom';
+import {useNavigate, Link, useLocation} from 'react-router-dom';
 const Header = () => {
+  const {search} = useLocation;
+  const redirectInUrl = new URLSearchParams(search).get('redirect');
+  const redirect = redirectInUrl ? redirectInUrl : '/';
   return (
     <header className="flex items-center w-full mb-6 md:py-2 justify-between   shadow-2xl shadow-cyan-500/50   md:px-0 bg-primary">
       
@@ -16,7 +19,7 @@ const Header = () => {
                                                       
                             <li className='mx-5 text-textColor  text-lg' ><small >Orders</small> </li>
                             <li className='mx-5 text-textColor  text-lg' ><small >Contact-us</small> </li> 
-                            <li className='mx-5 text-textColor text-lg'> <small >Logout</small>  </li>                              
+                            <li className='mx-5 text-textColor text-lg'> <small >   <Link to={`/login?redirect=${redirect}`}>LogOut</Link></small>  </li>                              
                              <img src={profile} className=' w-12 h-12 min-w[44px] object-cover rounded-full shadow-lg' alt="user-pic" />             
                         </ul> 
                         
